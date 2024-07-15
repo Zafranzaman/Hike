1f1bf0bb2cc83eec48bceca2dce2e06b50cdf233
Zafran <mzafransultani56@gmail.com>
2024-07-15 10:11:41 +0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Zafran on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
